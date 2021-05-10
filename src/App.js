a7bd33066da3bcb1ec5fd80b46cc385cb7d8dd9b
jhagg85 <64770626+jhagg85@users.noreply.github.com>
2021-05-10 23:54:55 +0800

    finalizing for app build

--- src/App.js
+++ src/App.js
@@ -1,35 +1,41 @@
+import axios from "./axios";
+import { useEffect, useState } from "react";
 import "./App.css";
 import Video from "./Video";
 
 function App() {
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    async function fetchPosts() {
+      const response = await axios.get("/v2/posts");
+      setVideos(response.data);
+
+      return response;
+    }
+
+    fetchPosts();
+  }, []);
+
+  console.log(videos);
+
   return (
     <div className="app">
       <div className="app__videos">
-        <Video
-          url="video.com"
-          channel="jhagg_"
-          description="this is the TIKTOK MERN build"
-          song="Honesty - Hallman"
-          likes={100}
-          shares={10}
-          messages={50}
-        />
-        <Video
-          url="video.com"
-          channel="jhagg_"
-          description="this is the TIKTOK MERN build"
-          song="Honesty - Hallman"
-          likes={100}
-          shares={10}
-          messages={50}
-        />
+        {videos.map(
+          ({ url, channel, description, song, likes, messages, shares }) => (
+            <Video
+              url={url}
+              channel={channel}
+              song={song}
+              likes={likes}
+              messages={messages}
+              description={description}
+              shares={shares}
+            />
+          )
+        )}
       </div>
-
-      {/* App container */}
-      {/* <Video /> */}
-      {/* <Video /> */}
-      {/* <Video /> */}
-      {/* <Video /> */}
     </div>
   );
 }
